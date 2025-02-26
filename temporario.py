ec7d864196de2fce819973b1b4a3d59c67a976e5
Fernando de Gois <fernando.gois90@outlook.com>
2025-02-26 20:33:20 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -5,7 +5,7 @@ st.text_input("Titulo do filme", "É o Brian!")
 modo_carnaval = st.toggle("Modo Carnaval")
 if modo_carnaval:
     st.write("🎉🎶")
-    st.ballons("")
+    st.balloons("")
 else:
     st.write("Modo Carnaval desativado. Que pena! 😢")
   
