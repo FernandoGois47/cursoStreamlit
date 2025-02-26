112160d68a2a35cf4ec68f727ed27e7330837abd
Fernando de Gois <fernando.gois90@outlook.com>
2025-02-26 19:56:40 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -2,7 +2,6 @@ import streamlit as st
 st.header("Bem Vindo ao mundo mágico do Gois")
 "Toquem no botão para não salvar 👇"
 st.button("Botão Salvar")
-st.text_area("Enter Text")
 st.text_input("Movie title", "Life of Brian")
 st.toggle("Toggle")
 
