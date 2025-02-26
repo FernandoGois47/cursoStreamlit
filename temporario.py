9110382223dad39b5d58fbc3c60b2c19a1e64821
Fernando de Gois <fernando.gois90@outlook.com>
2025-02-26 19:51:59 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -1,5 +1,16 @@
 import streamlit as st
-st.header("Cabeçalho")
+st.header("Bem Vindo ao mundo mágico do Gois")
+Toquem no botão para não salvar 👇
 st.button("Botão Salvar")
+st.text_area("Enter Text")
+st.text_input("Movie title", "Life of Brian")
 st.toggle("Toggle")
 
+st.radio(
+  "Ta bonito? 👇",
+  ["Ééééé....", "Meio boca", "Pagar uma caixa de chocolate"],
+  key="visibility",
+  label_visibility=st.session_state.visibility,
+  disabled=st.session_state.disabled,
+  horizontal=st.session_state.horizontal,
+    )
