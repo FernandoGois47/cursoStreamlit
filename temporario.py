da603eff19eaa30f59b3a6f79d3d41ee6406cf09
Fernando de Gois <fernando.gois90@outlook.com>
2025-02-26 19:55:32 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -6,11 +6,7 @@ st.text_area("Enter Text")
 st.text_input("Movie title", "Life of Brian")
 st.toggle("Toggle")
 
-st.radio(
+genre = st.radio(
   "Ta bonito? 👇",
   ["Ééééé....", "Meio boca", "Pagar uma caixa de chocolate"],
-  key="visibility",
-  label_visibility=st.session_state.visibility,
-  disabled=st.session_state.disabled,
-  horizontal=st.session_state.horizontal,
     )
