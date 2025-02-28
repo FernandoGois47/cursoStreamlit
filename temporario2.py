79aad2b536f070fc47ed96cb13edd0d132dc5d36
Fernando de Gois <fernando.gois90@outlook.com>
2025-02-28 19:49:32 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -10,10 +10,10 @@ A, B, C = st.columns(3)
 with A:
   st.header("Gatinho")
   st.image("https://jpimg.com.br/uploads/2025/01/10-curiosidades-sobre-os-filhotes-de-gato.jpg")
-with A:
+with B:
   st.header("Catchoro")
   st.image("https://denisemazzola.com/wp-content/uploads/Dog-Psychology.svg")
-with A:
+with C:
   st.header("Digimon")
   st.image("https://m.media-amazon.com/images/M/MV5BNjk1NTQ4NmEtZWM5MC00OTUwLTk2YTItOGU5OTVmNGEyNGNhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg")
   
