f1705524d5540f51b6c741dc46b53ffa8a30f700
Fernando de Gois <fernando.gois90@outlook.com>
2025-02-28 20:38:33 -0300

    Update modelagem.py

--- modelagem.py
+++ modelagem.py
@@ -14,7 +14,8 @@ with A:
   jan_1 = datetime.date(next_year, 1, 1)
   dec_31 = datetime.date(next_year, 12, 31)
   
-  title = st.text_input("Data de Nascimento:", format="DD.MM.YYYY",
+  st.text_input("Data de Nascimento:", 
+                format="DD.MM.YYYY",
   )
   
 
