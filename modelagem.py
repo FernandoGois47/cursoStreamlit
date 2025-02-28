e89464fe292c7d0263fe9e904902ed7d4efb7195
Fernando de Gois <fernando.gois90@outlook.com>
2025-02-28 20:15:55 -0300

    Update modelagem.py

--- modelagem.py
+++ modelagem.py
@@ -3,6 +3,8 @@ import streamlit as st
 st.title("Aluno")
 
 A, B = st.columns(2)
+
 st.subheader("Editar Dados")
+
 with A:
-title = st.text_input("Nome Completo")
+  title = st.text_input("Nome Completo")
