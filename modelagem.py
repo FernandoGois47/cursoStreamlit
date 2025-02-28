f8f7bfc62411ccb270d8db87f3a52514b109e570
Fernando de Gois <fernando.gois90@outlook.com>
2025-02-28 20:26:05 -0300

    Update modelagem.py

--- modelagem.py
+++ modelagem.py
@@ -7,4 +7,20 @@ A, B = st.columns(2)
 with A:
   st.subheader("Editar Dados")
   title = st.text_input("Nome Completo")
-  title = st.text_input("Data de Nascimento")
+  title = st.text_input("Data de Nascimento:")
+
+  st.selectbox(
+    "Sexo:",
+    ("Masculino", "Feminino", "Outros"),
+  )
+
+  title = st.text_input("RG:")
+  title = st.text_input("Nome do Pai:")
+  title = st.text_input("Nome do Mãe:")
+  title = st.text_input("E-mail:")
+
+with B:
+  title = st.text_input("Logradouro:")
+  title = st.text_input("Número:")
+  title = st.text_input("Bairro:")
+  title = st.text_input("CEP:")
