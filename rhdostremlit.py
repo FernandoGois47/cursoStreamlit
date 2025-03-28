b94a134bf1287f5a6ec9edd1b7300770137ca143
Fernando de Gois <fernando.gois90@outlook.com>
2025-03-28 20:33:19 -0300

    Update rhdostremlit.py

--- rhdostremlit.py
+++ rhdostremlit.py
@@ -1,24 +1,27 @@
 import streamlit as st
 import datetime
 
+
+with st.container(True):
 st.title("Funcionario")
 
 A, B = st.columns(2)
 
 with A:
   st.subheader("Editar Dados")
-  title = st.text_input("Nome")
-  title = st.text_input("Sobrenome")
+  nome = st.text_input("Nome")
+  sobrenome = st.text_input("Sobrenome")
 
   d = st.date_input("Qual sua data de nascimento", datetime.date(2025, 28, 3))
 
-  st.selectbox(
+  estadocivil = st.selectbox(
     "Estado Civil:",
     ("Solteiro", "Casado", "Divorciado", "Viúvo"),
   )
-  st.selectbox(
+  sexo = st.selectbox(
     "Sexo:",
     ("Masculino", "Feminino", "Outros"),
   )
 
 with B:
+  salario = st.number_input("Qual seu salário")
