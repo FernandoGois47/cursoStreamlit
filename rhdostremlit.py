b193604a1284db612d29ae3fbcd6b75761851cb4
Fernando de Gois <fernando.gois90@outlook.com>
2025-03-28 20:38:41 -0300

    Update rhdostremlit.py

--- rhdostremlit.py
+++ rhdostremlit.py
@@ -3,22 +3,22 @@ import datetime
 
 st.title("Funcionario")
 
-A, B = st.columns(2)
 
-with A:
-  st.subheader("Editar Dados")
-  nome = st.text_input("Nome")
-  sobrenome = st.text_input("Sobrenome")
 
-  niver = st.date_input("Qual sua data de nascimento", datetime.date.today())
+st.subheader("Editar Dados")
+nome = st.text_input("Nome")
+sobrenome = st.text_input("Sobrenome")
 
-  estadocivil = st.selectbox(
-    "Estado Civil:",
-    ("Solteiro", "Casado", "Divorciado", "Viúvo"),
-  )
-  sexo = st.selectbox(
-    "Sexo:",
-    ("Masculino", "Feminino", "Outros"),
-  )
-with B:
-  salario = st.number_input("Qual seu salário")
+niver = st.date_input("Qual sua data de nascimento", datetime.date.today())
+
+estadocivil = st.selectbox(
+  "Estado Civil:",
+  ("Solteiro", "Casado", "Divorciado", "Viúvo"),
+)
+sexo = st.selectbox(
+  "Sexo:",
+  ("Masculino", "Feminino", "Outros"),
+)
+salario = st.number_input("Qual seu salário")
+
+
