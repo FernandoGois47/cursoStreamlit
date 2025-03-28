191771cb099af7aa5e2ef36b5d1a9c1903a3d0ec
Fernando de Gois <fernando.gois90@outlook.com>
2025-03-28 20:36:56 -0300

    Update rhdostremlit.py

--- rhdostremlit.py
+++ rhdostremlit.py
@@ -10,7 +10,7 @@ with A:
   nome = st.text_input("Nome")
   sobrenome = st.text_input("Sobrenome")
 
-  niver = st.date_input("Qual sua data de nascimento", datetime.date(2025, 28, 3))
+  niver = st.date_input("Qual sua data de nascimento", datetime.date.today())
 
   estadocivil = st.selectbox(
     "Estado Civil:",
