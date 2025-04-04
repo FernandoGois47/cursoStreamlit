3ea8900b71dd6fc393e909066bb0fc89b29afc86
Fernando de Gois <fernando.gois90@outlook.com>
2025-04-04 19:29:59 -0300

    Update rhdostremlit.py

--- rhdostremlit.py
+++ rhdostremlit.py
@@ -19,17 +19,18 @@ sexo = st.selectbox(
 )
 salario = st.number_input("Qual seu salário")
 
-st.button ("Calcular")
-  if salario >= 2500:
-    st.write("Noooosssaaa, vai rola aumento! Parabéns!")
-    aumento = 0
-    while aumento > 500:
-      aumento = aumento + 100
-      salario = aumento
-      st.write("Seu novo salário é: ")
-      st.write(salario)
-  else:
-    st.write("Não foi dessa vez")
+
+if  st.button ("Calcular") salario >= 2500:
+  st.write("Noooosssaaa, vai rola aumento! Parabéns!")
+  aumento = 0
+  while aumento > 500:
+    aumento = aumento + 100
+    salario = aumento
+else:
+  st.write("Não foi dessa vez")
+
+st.write("Seu novo salário é: ")
+st.write(salario)
 
 
 
